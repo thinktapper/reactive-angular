65d230a33250af468d117695a88bc504a813b1e5
Andrew Tapper <andrew@tapper.codes>
2023-03-14 15:35:33 -0500

    :poop: basics

--- libs/core-data/src/lib/state/projects/projects.actions.ts
+++ libs/core-data/src/lib/state/projects/projects.actions.ts
@@ -1,39 +1,50 @@
-import { Action } from '@ngrx/store';
+import { Action } from '@ngrx/store'
 
-import { Project } from './../../projects/project.model';
+import { Project } from './../../projects/project.model'
 
 export enum ProjectsActionTypes {
   ProjectSelected = '[Projects] Selected',
   LoadProjects = '[Projects] Load Data',
+  ProjectsLoaded = '[Projects] Data Loaded',
   AddProject = '[Projects] Add Data',
+  ProjectAdded = '[Projects] Data Added',
   UpdateProject = '[Projects] Update Data',
   DeleteProject = '[Projects] Delete Data',
 }
 
 export class SelectProject implements Action {
-  readonly type = ProjectsActionTypes.ProjectSelected;
+  readonly type = ProjectsActionTypes.ProjectSelected
   constructor(private payload: Project) {}
 }
 export class LoadProjects implements Action {
-  readonly type = ProjectsActionTypes.LoadProjects;
+  readonly type = ProjectsActionTypes.LoadProjects
+}
+export class ProjectsLoaded implements Action {
+  readonly type = ProjectsActionTypes.ProjectsLoaded
   constructor(private payload: Project[]) {}
 }
 export class AddProject implements Action {
-  readonly type = ProjectsActionTypes.AddProject;
+  readonly type = ProjectsActionTypes.AddProject
+  constructor(private payload: Project) {}
+}
+export class ProjectAdded implements Action {
+  readonly type = ProjectsActionTypes.ProjectAdded
   constructor(private payload: Project) {}
 }
 export class UpdateProject implements Action {
-  readonly type = ProjectsActionTypes.UpdateProject;
+  readonly type = ProjectsActionTypes.UpdateProject
   constructor(private payload: Project) {}
 }
 export class DeleteProject implements Action {
-  readonly type = ProjectsActionTypes.DeleteProject;
+  readonly type = ProjectsActionTypes.DeleteProject
   constructor(private payload: Project) {}
 }
 
-export type ProjectsActions = SelectProject
+export type ProjectsActions =
+  | SelectProject
   | LoadProjects
+  | ProjectsLoaded
   | AddProject
+  | ProjectAdded
   | UpdateProject
   | DeleteProject
-  ;
